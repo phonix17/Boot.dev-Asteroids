0828bff12c8dbad19b51a228d70382b0dde09675
phonix17 <phynix17@gmail.com>
2025-08-28 18:17:43 -0700

    Added creation of player, movement, and asteroid generation

--- main.py
+++ main.py
@@ -2,20 +2,40 @@
 import pygame
 # Imports of other project files
 from constants import *
+from circleshape import CircleShape
+from player import player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable)
+
 def main():
     pygame.init()
     clock = pygame.time.Clock() # Define the clock for later use controlling framerate
     dt = 0 # DeltaTime, used along side the clock
-    # Initialization messages, useful for confirming constants
+    Player = player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # Create the player object
+    field = AsteroidField()
+    
+    # Initialization messages, useful for confirming setup functioning
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Use constants to define the play area
+    
     while True: # Main game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # Closes the game when the window closes, preventing need to also cancel from the terminal
                 return
         screen.fill("black") # Color play area
+        for objects in drawable:
+            objects.draw(screen) # Render the player object
+        updatable.update(dt) # Updates rotation of player based on pressed keys
         pygame.display.flip() # Displays updated state in the play area
         dt = clock.tick(60) / 1000 # Controls framerate and updates the DeltaTime
 
---
+++ player.py
@@ -0,0 +1,38 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+
+class player(CircleShape):
+    def __init__(self, x, y): # Create player object using Circle class as base; PLAYER_RADIUS comes from constants.py
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self): # Method taken from  boot.dev https://www.boot.dev/lessons/c504a055-69b9-49a9-a9cf-40a30556af92
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen): # Override of method from Circle class
+            pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt): # Defines rotation amount of the player using the DeltaTime and constant turn speed
+         self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def move(self, dt): # Allows forward/backward motion based on current rotation; vector math provided courtesy of Boot.dev https://www.boot.dev/lessons/f3c2de4d-3646-42c0-8639-71f0c4c01eaa
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt): # Moves player object if WASD pressed
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]: # Rotate left when "a" pressed
+            self.rotate(0 - dt)
+        if keys[pygame.K_d]: # Rotate right when "d" pressed
+            self.rotate(dt)
+        if keys[pygame.K_w]: # Rotate forward when "w" pressed
+            self.move(dt)
+        if keys[pygame.K_s]: # Rotate backward when "d" pressed
+            self.move(0 - dt)
