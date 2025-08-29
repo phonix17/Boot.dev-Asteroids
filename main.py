97a9f31e6c6c5f43bd832372294a802a86fa3fcb
phonix17 <phynix17@gmail.com>
2025-08-29 00:42:11 -0700

    Updated main to include scoring and multiple lives

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ def main():
     dt = 0 # DeltaTime, used along side the clock
     Player = player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # Create the player object
     field = AsteroidField()
+    score_counter = 0
+    player_lives = 3
     
     # Initialization messages, useful for confirming setup functioning
     print("Starting Asteroids!")
@@ -33,6 +35,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Use constants to define the play area
     
     while True: # Main game loop
+        if player_lives == 0:
+            print("Game Over!")
+            print(f"Score: {score_counter}")
+            exit()
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # Closes the game when the window closes, preventing need to also cancel from the terminal
                 return
@@ -42,13 +48,18 @@ def main():
         updatable.update(dt) # Updates all objects - spawning asteroids, moving player, shots and spawned asteroids
         for hazard in asteroids:
             if hazard.collision_check(Player):
-                print("Game Over!")
-                exit()
+                player_lives -= 1
+                for shots in bullets:
+                    shots.kill()
+                for hazard in asteroids:
+                    hazard.kill()
+                Player.position = pygame.Vector2((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
         for hazard in asteroids:
             for shots in bullets:
                 if shots.collision_check(hazard):
                     shots.kill()
                     hazard.split()
+                    score_counter += 1
         pygame.display.flip() # Displays updated state in the play area
         dt = clock.tick(60) / 1000 # Controls framerate and updates the DeltaTime
 
